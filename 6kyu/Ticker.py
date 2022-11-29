eb7753aeed3bcdebe0f37fc13f90a3fdd98c2b95
ryazd <67752025+ryazd@users.noreply.github.com>
2022-11-29 21:04:14 +0300

    Update Ticker.py

--- 6kyu/Ticker.py
+++ 6kyu/Ticker.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com//kata/5a959662373c2e761d000183
+
 def ticker(text, width, tick):
     tick = tick if tick < len(text) + width else tick % (len(text) + width)
     text = " " * width + text + " " * width
